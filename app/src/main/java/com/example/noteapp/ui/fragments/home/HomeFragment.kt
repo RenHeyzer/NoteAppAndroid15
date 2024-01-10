a9f1a6d7ab19da983863dfd4928798f5e55ed3b2
RenHeyzer <vesolyyradin@gmail.com>
2024-01-11 04:07:45 +0600

    OnBoard

---
+++ app/src/main/java/com/example/noteapp/ui/fragments/home/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.noteapp.ui.fragments.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/noteapp/ui/fragments/onboard/OnBoardPageSecond.kt
@@ -0,0 +1,33 @@
+package com.example.noteapp.ui.fragments.onboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentOnBoardPageSecondBinding
+
+class OnBoardPageSecond : Fragment(R.layout.fragment_on_board_page_second) {
+
+    private var _binding: FragmentOnBoardPageSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnBoardPageSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
